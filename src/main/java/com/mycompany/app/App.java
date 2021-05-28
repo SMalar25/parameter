012a2725a4a0ea2385bcc402a2d10a7619f7e384
Malar23 <malarkodishunmugavel@gmail.com>
2021-05-28 20:07:59 +0530

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+import java util.Scanner;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int n1,n2,sum;
+        Scanner s = new Scanner(System.in);
+        n1=s.nextInt();
+        n2=s.nextInt();
+        sum=n1+n2;
+        System.out.println( "Hello World!" );
+        System.out.println( "Sum of Number:"+sum);
+    }
+}
